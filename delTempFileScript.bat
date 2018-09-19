d1e366a6e3dc9cc8b48df3538266b2f1214c77d8
chenlin <37610940+a013152@users.noreply.github.com>
2018-09-19 22:26:19 +0800

    20180919-03
    删除VS2013生成的临时文件。

---
+++ delTempFileScript.bat
@@ -0,0 +1,8 @@
+del *.tlog /f /s /q /a
+del *.obj /f /s /q /a
+del *.log /f /s /q /a
+del *.pch /f /s /q /a
+del *.idb /f /s /q /a
+del *.pdb /f /s /q /a
+del *.ilk /f /s /q /a
+del *.sdf /f /s /q /a
